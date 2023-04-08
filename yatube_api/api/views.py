f747aa53c76f955357f28e78f9183d72672c76bc
Свешников Максим <maksworked@ya.ru>
2023-04-08 17:55:10 +0300

    flake8 fix

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from posts.models import Comment, Follow, Group, Post, User
-from rest_framework import filters, permissions, viewsets, mixins
+from rest_framework import filters, mixins, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
 from .permissions import AuthorOrReadOnly
